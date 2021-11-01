cca958fabb8e3633e992be7b5b49b7b3c452f37c
Rufo138 <rufino_138@hotmail.com>
2021-11-01 12:28:13 -0300

    subiendo proyecto

---
+++ frontend/vistas/modulos/cabezote.php
@@ -0,0 +1,129 @@
+<!-- TOP-->
+<div class="container-fluid barraSuperior" id="top">
+    <div class="container">
+        <div class="row">
+            <!-- REDES SOCIALES-->
+            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/dulces.momentos.7777" target="_blank">
+                            <i class="fa fa-facebook redSocial facebookColor" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://youtube.com/" target="_blank">
+                            <i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.instagram.com/" target="www.google.com.ar">
+                            <i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- REGISTRO -->
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+                <ul>
+                    <li><a href="#modalIngreso" data-toggle="modal">Ingresar</a></li>
+                    <li>|</li>
+                    <li><a href="#modalRegistro" data-toggle="modal">Crear una cuenta</a></li>
+
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- HEADER -->
+<header class="container-fluid">
+    <div class="container">
+        <div class="row" id="cabezote">
+            <!-- LOGOTIPO -->
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+                <a href="#">
+                    <img src="http://localhost/backend/vistas/img/plantilla/logo4.png" class="img-responsive" alt="Dulces Momentos general rodriguez">
+                </a>
+            </div>
+            <!-- CATEGORIAS Y BUSCARDOR -->
+
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+                <!-- BOTON CATEGORIAS -->
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+                    <p>CATEGORIAS
+                        <span class="pull-right">
+                            <i class="fa fa-bars" aria-hidden="true"></i>
+                        </span>
+                    </p>
+                </div>
+                <!-- BUSCADOR -->
+                <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+                    <input type="search" name="buscar" class="form-control" placeholder="Buscar...">
+                    <span class="input-group-btn">
+                        <a href="#">
+                            <button class="btn btn-default backColor" type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <!-- CARRITO -->
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+                <a href="#">
+                    <button class="btn btn-default pull-left backColor">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </button>
+                </a>
+                <p>TU CESTA <span class="cantidadCesta"></span><br> ARG $ <span class="sumaCesta">20</span></p>
+            </div>
+        </div>
+        <!-- CATEGORIAS -->
+        <div class="col-xs-12 backColor">
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4><a href="#" class="pixelCategorias">Lorem ipsum</a></h4>
+                <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4><a href="#" class="pixelCategorias">Lorem ipsum</a></h4>
+                <hr>
+                <ul>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4><a href="#" class="pixelCategorias">Lorem ipsum</a></h4>
+                <hr>
+                <ul>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4><a href="#" class="pixelCategorias">Lorem ipsum</a></h4>
+                <hr>
+                <ul>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="" class="pixelSubCategorias">Lorem ipsum</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ frontend/vistas/plantilla.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0,maximum-scale=1.0, user-scalable=no">
+    <meta name="title" content="Dulces Momentos">
+    <meta name="description" content="Pasteleria de excelente calidad, con materia prima de primer nivel, delicias para tu vida.">
+    <meta name="keyword" content="Pasteleria, tortas general rodriguez, dulces momentos tortas">
+    <title>Dulces Momentos</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+    <link rel="stylesheet" href="vistas/css/plantilla2.css">
+    <link rel="stylesheet" href="vistas/css/cabezote.css">
+    <script src="vistas/js/plugins/jquery.min.js"></script>
+    <script src="vistas/js/plugins/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <?php
+    /*=============================================
+=            Header            =
+=============================================*/
+    include "modulos/cabezote.php";
+    ?>
+
+
+</body>
+
+</html>
